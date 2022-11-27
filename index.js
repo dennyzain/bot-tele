cdf467c7a71b0877485ac6f9d85437e0829ec4e2
dennyzain <abbasdenny24@gmail.com>
2022-11-27 20:48:34 +0700

    remove credential

--- index.js
+++ index.js
@@ -2,9 +2,6 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
 const PORT = 8000;
-const teleUrl = 'bot5535531507:AAFLXmdmpb5y8g4A_ZNC7IPxPYWgqa_vlEk';
-const teleBaseUrl = 'https://api.telegram.org/';
-const teleMethod = '/getUpdates';
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
