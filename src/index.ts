2332bad1456c53154a66b4ee5c0893737c4bbfdc
dennyzain <abbasdenny24@gmail.com>
2022-12-18 15:09:14 +0700

    Connect to bot with telegraf
    - install telegraf, dotenv, setup eslint/config
    - config the package.json to module type
    - setup tsconfig supports top level await
    - remove express setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log('hello world!')
+import * as dotenv from 'dotenv';
+import {Telegraf} from 'telegraf';
+dotenv.config();
+
+const botToken = process.env.BOT_TOKEN ?? '';
+const bot = new Telegraf(botToken);
+
+bot.start(async ctx => {
+	const sender = ctx.update.message.from;
+	console.log(ctx.update.message.chat, ' ini chatt');
+	return ctx.reply(`
+	Welcome ${sender.first_name ?? 'anonymous'} ${sender.last_name ?? ''}
+	id telegram : ${sender.id} 
+	username : ${sender.username ?? 'anonymous'}`);
+});
+bot.help(async ctx => ctx.reply('Send me a sticker'));
+bot.on('message', async ctx => ctx.reply('testt'));
+
+await bot.launch();
+
